bd17689da5617c06792c6a2116d8dfc33d61edfb
mohamed-alaa <muhammadalaa84@gmail.com>
2023-03-23 13:09:40 +0200

    created the Intro

---
+++ src/components/Intro.js
@@ -0,0 +1,13 @@
+function Intro() {
+    return(
+        <div className="info">
+            <h2>My journey to become a React developer!</h2>
+            <div>
+                <p>Trying to improve my career by learning the react basic course</p>
+                <p className="link">Read more...</p>
+            </div>
+        </div>
+    );
+};
+
+export default Intro;
