57e0ec14b103f167796d536e62f2edba339952cc
mohamed-alaa <muhammadalaa84@gmail.com>
2023-03-23 12:55:09 +0200

    created the Promo

---
+++ src/components/Promo.js
@@ -0,0 +1,14 @@
+function Promo() {
+    return(
+        <div className="promo-section">
+            <div>
+                <h1>Don't miss this deal!</h1>
+            </div>
+            <div>
+                <h2>Subscribe now and get 50% off!</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Promo;
